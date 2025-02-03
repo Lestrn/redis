2b2ffa26916bfd5af4ff2f53d48974d9b29a91ac
Lestrn <ilestrn@gmail.com>
2025-02-03 14:16:35 +0200

    added tests front end for all crud

--- test/web_test/table_page_test.exs
+++ test/web_test/table_page_test.exs
@@ -1,11 +1,12 @@
 defmodule Redis.TablePageTest do
-  use RedisWeb.ConnCase, async: true
+  use RedisWeb.ConnCase, async: false
+  alias Redis.Repository.RedisRepo
   import Phoenix.LiveViewTest
-  @table_id "redis-table"
 
   describe "Web test" do
     setup do
       conn = build_conn()
+      RedisRepo.insert("key_to_change", "gggg")
       {:ok, view, _html} = live(conn, ~p"/table")
       {:ok, conn: conn, view: view}
     end
@@ -22,18 +23,55 @@ defmodule Redis.TablePageTest do
       assert render(view) =~ "Add new field to redis"
 
       view
-      |> form("form#insert_form",
+      |> form("form#insert-form",
         redis_schema: %{
-          key: "key22",
-          value: "value22"
+          key: "inserted_key",
+          value: "inserted_value"
         }
       )
       |> render_change()
 
-      view |> form("form#insert_form") |> render_submit()
+      view |> form("form#insert-form") |> render_submit()
 
-      assert render(view) =~ "key22"
-      assert render(view) =~ "value22"
+      assert render(view) =~ "inserted_key"
+      assert render(view) =~ "inserted_value"
+    end
+
+    test "update field event", %{view: view} do
+      view
+      |> element("button[phx-click='set_open_change_dialog'][value='key_to_change']", "Change")
+      |> render_click()
+
+      assert render(view) =~ "Update field"
+
+      view
+      |> form("form#update-form",
+        redis_schema: %{
+          key: "key_to_change",
+          value: "updated_value"
+        }
+      )
+      |> render_change()
+
+      view |> form("form#update-form") |> render_submit()
+
+      assert render(view) =~ "key_to_change"
+      assert render(view) =~ "updated_value"
+    end
+
+    test "delete field event", %{view: view} do
+      view
+      |> element("button[phx-click='set_open_change_dialog'][value='key_to_change']", "Change")
+      |> render_click()
+
+      assert render(view) =~ "Update field"
+
+      view
+      |> element("button[phx-click='submit_delete']", "Delete")
+      |> render_click()
+
+      refute render(view) =~ "key_to_change"
+      refute render(view) =~ "updated_value"
     end
   end
 end
