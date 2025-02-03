ab33bdbc54fb36cc57735ed39aba0cd9464f8634
Lestrn <ilestrn@gmail.com>
2025-02-03 14:44:24 +0200

    removed io inspect

--- lib/redis/schemas/redis_schema.ex
+++ lib/redis/schemas/redis_schema.ex
@@ -22,7 +22,6 @@ defmodule Redis.Schemas.RedisSchema do
     |> validate_required([:key, :value])
     |> validate_length(:key, max: 15)
     |> validate_length(:value, max: 15)
-    |> IO.inspect()
     |> validate_key_doesnt_exist()
   end
 
